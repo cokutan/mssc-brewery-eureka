6c5551d5c9a6ac102126e26fb5252a94f1123be3
cagla.boynuegri <cagla.boynuegri@scalefocus.com>
2024-12-30 17:45:52 +0300

    updated eureka to work from spring 3.x

--- src/main/java/guru/sfg/brewery/eureka/msscbreweryeureka/config/SecurityConfig.java
+++ src/main/java/guru/sfg/brewery/eureka/msscbreweryeureka/config/SecurityConfig.java
@@ -1,21 +1,26 @@
 package guru.sfg.brewery.eureka.msscbreweryeureka.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 /**
  * Created by jt on 5/16/20.
  */
 @Configuration
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-                .authorizeRequests()
-                .anyRequest().authenticated()
-                .and()
-                .httpBasic();
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable()) // Disable CSRF protection
+                .authorizeHttpRequests(authorize ->
+                        authorize.anyRequest().authenticated() // Require authentication for all requests
+                )
+                .httpBasic(withDefaults()); // Enable HTTP Basic authentication
+
+        return http.build();
     }
 }
