20f7d5370e889cad8fa9a6a5d77a5e52afd56dcd
cagla.boynuegri <cagla.boynuegri@scalefocus.com>
2025-01-02 12:27:45 +0300

    added missing annotaiton

--- src/main/java/guru/sfg/brewery/eureka/msscbreweryeureka/config/SecurityConfig.java
+++ src/main/java/guru/sfg/brewery/eureka/msscbreweryeureka/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package guru.sfg.brewery.eureka.msscbreweryeureka.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -11,6 +12,7 @@ import static org.springframework.security.config.Customizer.withDefaults;
  * Created by jt on 5/16/20.
  */
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
